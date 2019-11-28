472711d6b1239d80df3f82e2c9d13f13d4f47101
MouraYuri <mourayuri339@gmail.com>
2019-11-27 21:18:28 -0300

    tei tei

--- simplifica.py
+++ simplifica.py
@@ -8,11 +8,18 @@ def pega_unitaria(clausulas):
     for x in clausulas:
         if len(x) == 1: return x[0]
 
+def atualiza(clausulas, literal_unitario):
+
+
 def simplifica(clausulas):
     valoracao = {}
     while(tem_unitaria(clausulas)):
         literal_unitario = pega_unitaria(clausulas)
-        
+        if literal_unitario <0:
+            valoracao[literal_unitario] = True
+        else:
+            valoracao[literal_unitario] = False
+        clausulas
 
 clausulas = [[1, -3], [2, 3, -1], [-1]]
 
